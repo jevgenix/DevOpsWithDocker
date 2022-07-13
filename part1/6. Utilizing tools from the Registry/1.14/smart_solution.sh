4a046735705dc7d675a482df0806d58eabd42bd7
Smirnov Evgenii <evgenii@Smirnovs-MacBook-Pro.local>
2022-07-13 14:15:56 +0300

    2 started

--- part1/6. Utilizing tools from the Registry/1.14/smart_solution.sh
+++ part1/6. Utilizing tools from the Registry/1.14/smart_solution.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 # if we have all needed images already we can just use script to run everything. 
-docker run -d --rm -e  REACT_APP_BACKEND_URL="http://localhost:5000 npm run build" -p 8080:8080 frontend
+docker run -d --rm -e  REACT_APP_BACKEND_URL="http://localhost:5000 npm run build" -p 8000:8000 frontend
 docker run -d --rm -e  REQUEST_ORIGIN="http://localhost:8080" -p 5000:5000 backend
+
